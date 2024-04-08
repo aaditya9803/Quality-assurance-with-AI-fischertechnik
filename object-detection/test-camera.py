5bcf50a6b8cfa3548dd96cf185ee2640dd5c7d9e
Aaditya Neupane <aaditya9803@gmail.com>
2024-04-08 12:43:40 +0200

    camera issue fixed

--- object-detection/test-camera.py
+++ object-detection/test-camera.py
@@ -1,6 +1,9 @@
 import cv2
 import numpy as np
-import sys, getopt, time
+import sys
+import getopt
+import time
+import os
 
 try:
     SHOW_IMAGE = True
@@ -8,14 +11,12 @@ try:
 except ImportError:
     SHOW_IMAGE = False
     import tflite_runtime.interpreter as tflite
-    
 
 CAMERA_WIDTH = 320
 CAMERA_HEIGHT = 240
 
 
 def load_labels(label_path):
-    r"""Returns a list of labels"""
     with open(label_path) as f:
         labels = {}
         index = 0
@@ -26,26 +27,18 @@ def load_labels(label_path):
 
 
 def load_model(model_path):
-    r"""Load TFLite model, returns a Interpreter instance."""
     interpreter = tflite.Interpreter(model_path=model_path, num_threads=4)
     interpreter.allocate_tensors()
     return interpreter
 
 
 def process_image(interpreter, image, input_index):
-    r"""Process an image, Return a list of detected class ids and positions"""
-    input_data = np.expand_dims(image, axis=0)  # expand to 4-dim
+    input_data = np.expand_dims(image, axis=0)
 
-    # Process
     interpreter.set_tensor(input_index, input_data)
     interpreter.invoke()
 
-    # Get outputs
     output_details = interpreter.get_output_details()
-    # output_details[0] - position
-    # output_details[1] - class id
-    # output_details[2] - score
-    # output_details[3] - count
 
     positions = np.squeeze(interpreter.get_tensor(output_details[0]["index"]))
     classes = np.squeeze(interpreter.get_tensor(output_details[1]["index"]))
@@ -61,22 +54,15 @@ def process_image(interpreter, image, input_index):
 
 
 def display_result(result, frame, labels):
-    r"""Display Detected Objects"""
     font = cv2.FONT_HERSHEY_SIMPLEX
     size = 0.6
-    color = (255, 0, 0)  # Blue color
+    color = (255, 0, 0)
     thickness = 1
 
-    # let's resize our image to be 150 pixels wide, but in order to
-    # prevent our resized image from being skewed/distorted, we must
-    # first calculate the ratio of the *new* width to the *old* width
     r = 640.0 / frame.shape[1]
     dim = (640, int(frame.shape[0] * r))
     frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
 
-    # position = [ymin, xmin, ymax, xmax]
-    # x * IMAGE_WIDTH
-    # y * IMAGE_HEIGHT
     width = frame.shape[1]
     height = frame.shape[0]
 
@@ -96,31 +82,31 @@ def display_result(result, frame, labels):
 
 
 def main(argv):
+    dir_path = None
 
-    dir = None
-    
     try:
-        
-        opts, _ = getopt.getopt(argv, "hd:", ["help","directory="])
+        opts, _ = getopt.getopt(argv, "hd:", ["help", "directory="])
         for opt, arg in opts:
-            if opt == "-h, --help":
+            if opt == "-h" or opt == "--help":
                 raise Exception()
             elif opt in ("-d", "--directory"):
-                dir = str(arg)
+                dir_path = arg
 
-        if (dir is None ):
-            raise Exception()
+        if not dir_path:
+            raise Exception("Please specify a directory using -d or --directory option.")
 
-    except Exception:
-        print("Specify a directory in which the model is located.")
-        print("test-camera.py -d <directory>")
+        if not os.path.isdir(dir_path):
+            raise Exception("Directory '{}' does not exist.".format(dir_path))
+
+    except Exception as e:
+        print("Error:", e)
+        print("Usage: test-camera.py -d <directory>")
         sys.exit(2)
 
+    model_path = os.path.join(dir_path, "model.tflite")
+    label_path = os.path.join(dir_path, "labels.txt")
 
-    model_path = dir + "/model.tflite"
-    label_path = dir + "/labels.txt"
-
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # Use DirectShow as a backup if MSMF fails
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, CAMERA_WIDTH)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, CAMERA_HEIGHT)
     cap.set(cv2.CAP_PROP_FPS, 15)
@@ -129,37 +115,27 @@ def main(argv):
     labels = load_labels(label_path)
 
     input_details = interpreter.get_input_details()
-
-    # Get Width and Height
     input_shape = input_details[0]["shape"]
     height = input_shape[1]
     width = input_shape[2]
-
-    # Get input index
     input_index = input_details[0]["index"]
 
-    # Process Stream
     while True:
-        
         ret, frame = cap.read()
 
         if ret:
-
             start = round(time.time() * 1000)
-            image = cv2.resize(frame,  (width, height))
-#            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            image = cv2.resize(frame, (width, height))
             stop = round(time.time() * 1000)
-            
             print("-------TIMING--------")
             print("resize image: {} ms".format(stop - start))
 
             start = round(time.time() * 1000)
             top_result = process_image(interpreter, image, input_index)
             stop = round(time.time() * 1000)
-
             print("process image: {} ms".format(stop - start))
-            
             print("-------RESULTS--------")
+
             for obj in top_result:
                 pos = obj["pos"]
                 _id = obj["_id"]
@@ -171,11 +147,13 @@ def main(argv):
 
             if SHOW_IMAGE:
                 display_result(top_result, frame, labels)
-
                 c = cv2.waitKey(1)
                 if c == 27:
                     break
 
+    cap.release()
+    cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
